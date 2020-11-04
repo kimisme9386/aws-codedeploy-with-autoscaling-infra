b1f95ec2480f7ad7490aadd2047e2b9440fa5316
Chris Yang <kimisme9386@gmail.com>
2020-11-04 16:21:26 +0800

    Remove command

--- lib/aws-codedeploy-autoscaling.ts
+++ lib/aws-codedeploy-autoscaling.ts
@@ -102,8 +102,6 @@ export default class AwsCodedeployAutoscaling extends cdk.Construct {
       parameters: {
         commands: [
           "apt -y install dialog apt-utils",
-          "echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections",
-          "apt-get install -y -q",
           "apt -y install software-properties-common",
           "add-apt-repository -y ppa:ondrej/php",
           "apt-get update",
